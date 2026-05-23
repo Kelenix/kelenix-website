9eb5dc089ef4a4e991206aa28e583a432264b2c6
leodev6 <djouakalionel4@gmail.com>
2026-05-23 12:22:11 +0200

    top

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -90,6 +90,7 @@ export default async function LocaleLayout({ children, params }: Props) {
   return (
     <html lang={locale} className="scroll-smooth" data-scroll-behavior="smooth">
       <head>
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
       </head>
